2bd61f50d13ce3ae9b977cd5ef1c592b16251a86
naokikotani <nok.naoki.tigers.415@icloud.com>
2022-02-08 01:06:11 +0900

    0207

---
+++ 2022_02/02_07.go
@@ -0,0 +1,21 @@
+// C021:暴風域にいますか？
+
+package main
+import "fmt"
+func main() {
+  var xc, yc, r_1, r_2 int
+	fmt.Scanf("%d %d %d %d", &xc, &yc, &r_1, &r_2)
+	var n int
+	fmt.Scanf("%d", &n)
+
+	for i := 0; i < n; i++ {
+		var x_i, y_i int
+		fmt.Scanf("%d %d", &x_i, &y_i)
+		sum := (x_i - xc) * (x_i - xc) + (y_i - yc) * (y_i - yc)
+		if r_1 * r_1 <= sum && sum <= r_2 * r_2 {
+		  fmt.Println("yes")
+		} else {
+		  fmt.Println("no")
+		}
+	}
+}
